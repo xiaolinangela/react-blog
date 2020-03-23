b5bcc3a6f9dc7a1e7166a7e82180f7d00fc6495b
Angela Zhu <xiaolinangela@gmail.com>
2020-03-22 21:22:11 -0700

    created action creators and reducers

---
+++ src/components/UserHeader.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import { fetchUser } from "../actions";
+
+class UserHeader extends Component {
+  //   componentDidMount() {
+  //     this.props.fetchUser(this.props.userId);
+  //   }
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+    return <div>{user.name}</div>;
+  }
+}
+
+const mapToStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapToStateToProps)(UserHeader);
